7a9c8298b342c4f4ec304d4f89ebd06c4aa80fd5
leon_hm <leon_hm@163.com>
2016-01-14 19:10:43 +0800

    commit project

---
+++ app/src/main/java/com/leon/statusbar/MainActivity.java
@@ -0,0 +1,16 @@
+package com.leon.statusbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        StatusBarUtils.setWindowStatusBarColor(this, android.R.color.holo_blue_bright);
+
+    }
+}
